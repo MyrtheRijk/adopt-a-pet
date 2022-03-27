5d312ef8f5def4b24450cbf1d6a16f48508eead6
MRijk <mjrijk@hotmail.com>
2022-03-27 19:08:11 +0200

    all files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/:type/:id">
+            <PetDetailsPage />
+          </Route>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/pet-details-not-found">
+            <PetDetailsNotFound />
+          </Route>
+          <Route path="/:type?">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/search/index.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Search = () => {
+  const history = useHistory();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = new URLSearchParams({
+      name: searchInputRef.current.value
+    }).toString();
+
+    history.push('/search?' + searchQuery);
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
